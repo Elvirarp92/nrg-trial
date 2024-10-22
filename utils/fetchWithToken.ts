b6fec2d06534aef1e5756c3af5e762a6be24ab4a
Elvira Ramírez <elvirarp92@gmail.com>
2024-10-22 17:31:37 +0200

    feat: add fetchWithToken() function
    To be used instead of fetch() when querying the ETRM API.
    Makes requests to the ETRM API including the bearer token from the
    current session.
    This way we don't have to import our authOptions and getServerSession
    everywhere we want to make a request.
    
    Tested manually.

---
+++ utils/fetchWithToken.ts
@@ -0,0 +1,39 @@
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+export enum HttpMethod {
+  GET = 'GET',
+  HEAD = 'HEAD',
+  POST = 'POST',
+  PUT = 'PUT',
+  PATCH = 'PATCH',
+  DELETE = 'DELETE',
+}
+
+export interface RequestOptions {
+  method?: HttpMethod,
+  body?: object,
+  queryParams?: string | string[][] | Record<string, string>
+}
+
+export default async function fetchWithToken(relativeUrl: string, options: RequestOptions): Promise<Response> {
+  const { method = HttpMethod.GET, body, queryParams } = { ...options }
+  const session = await getServerSession(authOptions)
+  const headers = {
+    "Content-Type": "application/json",
+    "Authorization": `Token ${session?.accessToken}`
+  }
+  const searchParams = new URLSearchParams(queryParams);
+  const fullUrl = new URL(`${process.env.API_URL}${relativeUrl}/?${searchParams.toString()}`)
+
+  const config: RequestInit = {
+    method,
+    headers
+  }
+
+  if (![HttpMethod.GET, HttpMethod.HEAD].includes(method) && !!body) {
+    config.body = JSON.stringify(body)
+  }
+
+  return fetch(fullUrl, config)
+}
