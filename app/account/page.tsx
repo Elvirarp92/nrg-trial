762d15951a28acd6ad6d77a707768679c03e14d0
Elvira Ramírez <elvirarp92@gmail.com>
2024-10-24 12:29:17 +0200

    feat: add /account view
    Shows account information and config options.

---
+++ app/account/page.tsx
@@ -0,0 +1,21 @@
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+
+export default async function Me() {
+  const session = await getServerSession(authOptions)
+
+  return (
+    <article className='grid gap-3'>
+      <h1>Your account</h1>
+      <h2>Overview</h2>
+      <ul>
+        <li>
+          <b>Name:</b> {session?.username}
+        </li>
+        <li>
+          <b>Staff:</b> {session?.isStaff ? 'Yes' : 'No'}
+        </li>
+      </ul>
+    </article>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import Link from 'next/link'
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
   return (
     <div>
       <h1>NRG Deal Manager</h1>
-      <p>Welcome, {session?.username}!</p>
+      <p>
+        Welcome,{' '}
+        <Link
+          className='text-secondary'
+          href='/account'
+        >
+          {session?.username}
+        </Link>
+        !
+      </p>
     </div>
   )
 }
